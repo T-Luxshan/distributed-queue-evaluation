ecc6fb10c4b45051a02048293b62a2e53bd47009
T-Luxshan <luckybraveboys@gmail.com>
2025-07-31 00:19:09 +0530

    Enqueue task

--- asynq/main.go
+++ asynq/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/T-Luxshan/distributed-queue-evaluation/asynq/tasks"
 	"github.com/hibiken/asynq"
 )
@@ -12,13 +13,18 @@ func asynqImp(client *asynq.Client) {
 	userID := 456
 	requestID := "req-456"
 
-	fare := 1500
+	fare := 100
 
 	tasks.EnqueueTask(client, userID, fare, requestID)
 }
 
 func main() {
 	client := asynq.NewClient(asynq.RedisClientOpt{Addr: redisAddr})
-	defer client.Close()
+	defer func(client *asynq.Client) {
+		err := client.Close()
+		if err != nil {
+			fmt.Println("Error closing redis client:", err)
+		}
+	}(client)
 	asynqImp(client)
 }
